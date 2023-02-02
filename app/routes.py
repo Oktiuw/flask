cf6cf6d8bea72c9be094632bf6131994b475463f
Aurélien Vincent <aurelien.vincent@etudiants.univ-reims.fr>
2023-02-02 09:00:03 +0100

    Ajout du formulaire d'enregisstreement

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
+from flask import render_template, flash, redirect
 
 
 @app.route('/')
@@ -17,6 +19,17 @@ def index() -> str:
         }
     ]
     return render_template('index.html', user=user, posts=posts)
+
+
 @app.route('/apropos')
-def apropos()->str:
+def apropos() -> str:
     return render_template('apropos.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"Enregistrement demandé pour l’utilisateur {form.username.data}, Se souvenir = {form.remember_me.data}")
+        return redirect('/index')
+    return render_template('login.html', title='Enregistrement', form=form)
