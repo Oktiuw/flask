14680fc1ee9bdfa3afa1e168f8e3f95d6af38808
Aurélien Vincent <aurelien.vincent@etudiants.univ-reims.fr>
2023-02-09 09:31:52 +0100

    fin de la partie connexion

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,9 @@
 # app/forms.py
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, EqualTo, Email
+
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -11,4 +13,22 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Enregistrer')
 
 
+class RegistrationForm(FlaskForm):
+    username = StringField(label='Utilisateur', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Mot de passe', validators=[DataRequired()])
+    password2 = PasswordField(label='Répéter le mot de passe',
+                              validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField("Enregistrer")
 
+    # Ajout d’une vérification sur le champ/attribut ‘username’
+    def validate_username(self: object, username: StringField) -> None:
+        user = User.query.filter(User.username == username.data).first()
+        if user is not None:
+            raise ValidationError("Choisissez un autre nom.")
+
+    # Ajout d’une vérification sur le champ/attribut ‘email’
+    def validate_email(self: object, email: StringField) -> None:
+        user = User.query.filter(User.email == email.data).first()
+        if user is not None:
+            raise ValidationError("Choisissez une autre adresse Email.")
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, Response
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from flask import render_template, flash, redirect, url_for
 from app.models import User
 from flask_login import current_user, login_user, logout_user, login_required
@@ -53,3 +53,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Bravo ! Vous devenez un nouvel utilisateur !')
+        return redirect(url_for('login'))
+    return render_template('register.html', title="S'enregistrer", form=form)
