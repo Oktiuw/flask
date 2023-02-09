2f15d6ec940b0318aeda97ff55241b3639a7c47b
Aurélien Vincent <aurelien.vincent@etudiants.univ-reims.fr>
2023-02-09 10:14:37 +0100

    Formatage de la date

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 # app/__init__.py
 from flask import Flask
+from flask_moment import Moment
+
 from app.config import Config
 
 from flask_sqlalchemy import SQLAlchemy
@@ -7,10 +9,9 @@ from flask_migrate import Migrate
 
 from flask_login import LoginManager
 
-
 # Création de l'application
 # __name__ contient le nom de l'application : app
-app = Flask( __name__ )
+app = Flask(__name__)
 app.config.from_object(Config)
 # Démarrage du moteur de la base de données
 db = SQLAlchemy(app)
@@ -21,9 +22,10 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 # Fonction de vue de redirection
 login.login_view = 'login'
+login.login_message = 'Il faut être connecté pour accèder à cette page'
 
+moment = Moment(app)
 # On importe le fichier contenant
 # la définition des fonctions de vue
 # ainsi que celui des modèles
 from app import routes, models
-
