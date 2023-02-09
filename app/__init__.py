4679b556f082b7a898854fff36c49f0815d4ef91
Aurélien Vincent <aurelien.vincent@etudiants.univ-reims.fr>
2023-02-09 11:10:27 +0100

    Fin de séance

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
 # app/__init__.py
+import logging
+from logging.handlers import SMTPHandler
+
 from flask import Flask
 from flask_moment import Moment
 
@@ -13,6 +16,20 @@ from flask_login import LoginManager
 # __name__ contient le nom de l'application : app
 app = Flask(__name__)
 app.config.from_object(Config)
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+    mail_handler = SMTPHandler(mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+    fromaddr = 'no-reply@' + app.config['MAIL_SERVER'],
+    toaddrs = app.config['ADMINS'], subject='Erreur dans MonApplication',
+    credentials=auth, secure=secure)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
 # Démarrage du moteur de la base de données
 db = SQLAlchemy(app)
 # Démarrage de l'outil de migration associé à la base de données
--- app/config.py
+++ app/config.py
@@ -1,10 +1,18 @@
 # app/config.py
 import os
 
-basedir = os.path.abspath(os.path.dirname( __file__ ))
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'un mot de passe à garder secret'
     # Configuration de la base de données
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Configuration d'un "pseudo" serveur mail
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['vinau02@gmail.com']
