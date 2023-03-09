d7172ff310e3042132c30becf1ecbf3581c44486
Aurélien Vincent <aurelien.vincent@etudiants.univ-reims.fr>
2023-03-09 09:06:06 +0100

    Création d'un 'app factory'

--- app/__init__.py
+++ app/__init__.py
@@ -18,52 +18,56 @@ from flask_login import LoginManager
 # __name__ contient le nom de l'application : app
 app = Flask(__name__)
 app.config.from_object(Config)
-
-if not app.debug:
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/mon_application.log', maxBytes=512000,backupCount=10)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s : %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('Démarrage de MonApplication')
-
-
-if not app.debug:
-    if app.config['MAIL_SERVER']:
-        auth = None
-        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-        secure = None
-        if app.config['MAIL_USE_TLS']:
-            secure = ()
-        mail_handler = SMTPHandler(mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-        fromaddr = 'no-reply@' + app.config['MAIL_SERVER'],
-        toaddrs = app.config['ADMINS'], subject='Erreur dans MonApplication',
-        credentials=auth, secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
-# Démarrage du moteur de la base de données
-db = SQLAlchemy(app)
-# Démarrage de l'outil de migration associé à la base de données
-migrate = Migrate(app, db)
-mail = Mail(app)
-# Instanciation du module de gestion des connexions
-login = LoginManager(app)
-# Fonction de vue de redirection
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
 login.login_view = 'auth.login'
-login.login_message = 'Il faut être connecté pour accèder à cette page'
-from app.auth import bp as auth_bp
-from app.erreurs import bp as erreurs_bp
-from app.main import bp as main_bp
-app.register_blueprint(erreurs_bp)
-app.register_blueprint(auth_bp)
-app.register_blueprint(main_bp)
-moment = Moment(app)
-# On importe le fichier contenant
-# la définition des fonctions de vue
-# ainsi que celui des modèles
+login.login_message = 'Veuillez vous connecter pour accéder à cette page.'
+mail = Mail()
+moment = Moment()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    from app.auth import bp as auth_bp
+    from app.erreurs import bp as erreurs_bp
+    from app.main import bp as main_bp
+    app.register_blueprint(erreurs_bp)
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(main_bp)
+    if not app.debug:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/mon_application.log', maxBytes=512000, backupCount=10)
+        file_handler.setFormatter(
+            logging.Formatter('%(asctime)s %(levelname)s : %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Démarrage de MonApplication')
+
+    if not app.debug:
+        if app.config['MAIL_SERVER']:
+            auth = None
+            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+                auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+            secure = None
+            if app.config['MAIL_USE_TLS']:
+                secure = ()
+            mail_handler = SMTPHandler(mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                                       fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+                                       toaddrs=app.config['ADMINS'], subject='Erreur dans MonApplication',
+                                       credentials=auth, secure=secure)
+            mail_handler.setLevel(logging.ERROR)
+            app.logger.addHandler(mail_handler)
+    return app
+
 
 from app import models
 from app.main import routes
